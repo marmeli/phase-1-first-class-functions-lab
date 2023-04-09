a8e7767dbf68eb9ed45295e5e4cff37b15adaecf
melissamariano <melissams538@gmail.com>
2023-04-09 16:58:28 -0400

    first class function

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Code your solution in this file!
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+const returnFirstTwoDrivers = (drivers) => {
+   return drivers.slice(0,2);
+} 
+
+const returnLastTwoDrivers = (drivers) => {
+    return drivers.slice(2,5);
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+const createFareMultiplier = (a) => {
+    return (fare) => a*a
+}
+
+function fareDoubler(createFareMultiplier){
+    return createFareMultiplier * 2;
+}
+function fareTripler(createFareMultiplier){
+    return createFareMultiplier * 3;
+}
+
+const selectDifferentDrivers = function(drivers, selectingDrivers){
+    return selectingDrivers(drivers);
+}
